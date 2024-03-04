30178f6b330c030fe8a239a96952a13558487551
Shova24 <jannatul35-2568@diu.edu.bd>
2024-03-04 12:36:53 +0600

    feat: navbar added to homepage layout

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,99 @@
+import { Button, Drawer, Dropdown, Select } from "antd";
+import { ChevronDown, Globe, Menu } from "lucide-react";
+import React, { useState } from "react";
+
+const items = [
+  {
+    key: "1",
+    label: <a href="/1st-menu-item">1st menu item</a>,
+  },
+  {
+    key: "2",
+    label: <a href="/2nd-menu-item">2nd menu item</a>,
+  },
+];
+
+const NavBar = () => {
+  const [open, setOpen] = useState(false);
+
+  const MenuItem = ({ title, isNested = false }) => {
+    return isNested ? (
+      <Dropdown
+        menu={{
+          items,
+        }}
+        placement="bottom"
+        arrow
+      >
+        <a
+          href={`/${title}`}
+          className="flex items-center  gap-1 cursor-pointer hover:text-[#4262FF]"
+        >
+          <span>{title}</span>
+          <span>
+            <ChevronDown className="w-4" />
+          </span>
+        </a>
+      </Dropdown>
+    ) : (
+      <a
+        href={`/${title}`}
+        className="flex items-center  cursor-pointer hover:text-[#4262FF]"
+      >
+        <span>{title}</span>
+      </a>
+    );
+  };
+
+  return (
+    <div className="flex items-center justify-center py-4 px-2 gap-4">
+      <img src="/miro.svg" alt="logo" className="h-6 w-20" />
+
+      <div className="flex-1 flex justify-between  ">
+        <div className="hidden lg:flex gap-4 ">
+          <MenuItem title="Products" isNested={true} />
+          <MenuItem title="Solutions" isNested={true} />
+          <MenuItem title="Resources" isNested={true} />
+          <MenuItem title="Enterprise" />
+          <MenuItem title="Pricing" />
+        </div>
+        <div className="hidden lg:flex gap-4">
+          <Select defaultValue="en" suffixIcon={null} className="border-0">
+            <Select.Option value="en">
+              <p className="flex items-center gap-2">
+                <Globe className="w-4 h-4" /> <span>EN</span>
+              </p>
+            </Select.Option>
+            <Select.Option value="bn">
+              <p className="flex items-center gap-2">
+                <Globe className="w-4 h-4" /> <span>BN</span>
+              </p>
+            </Select.Option>
+          </Select>
+          <MenuItem title="Contact" />
+          <MenuItem title="Sales" />
+          <MenuItem title="Login" />
+          <Button>Sign up free </Button>
+        </div>
+      </div>
+      {/* for smaller screen  */}
+      <div className="justify-end  flex lg:hidden">
+        <Menu className="h-8 cursor-pointer" onClick={() => setOpen(true)} />
+      </div>
+      <Drawer onClose={() => setOpen(false)} open={open}>
+        <div className="space-y-2">
+          <MenuItem title="Products" isNested={true} />
+          <MenuItem title="Solutions" isNested={true} />
+          <MenuItem title="Resources" isNested={true} />
+          <MenuItem title="Enterprise" />
+          <MenuItem title="Pricing" />
+          <MenuItem title="Contact" />
+          <MenuItem title="Sales" />
+          <MenuItem title="Login" />
+        </div>
+      </Drawer>
+    </div>
+  );
+};
+
+export default NavBar;
--- src/layouts/HomePageLayout.jsx
+++ src/layouts/HomePageLayout.jsx
@@ -1,11 +1,18 @@
-import React from "react";
 import { Outlet } from "react-router-dom";
+
+import NavBar from "../components/NavBar";
 
 const HomePageLayout = () => {
   return (
-    <div>
-      HomePageLayout
-      <Outlet />
+    <div className="bg-white ">
+      <div className="bg-white  border-b-[.05px]">
+        <NavBar />
+      </div>
+
+      <div className="min-h-screen container">
+        <Outlet />
+      </div>
+      <footer className="sticky bottom-0">d</footer>
     </div>
   );
 };
